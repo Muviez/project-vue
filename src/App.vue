f14540294e0eee5366e79c8429b77588a24b3f96
Andrey Goryushin <goryushin.andre@yandex.ru>
2018-12-05 23:40:25 +0300

    work with inputs

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div>
-    <h2>{{ title }}</h2>
+    <h2>From inputs</h2>
+    <input type="text" v-model="name">
 
-    <input type="text" v-model="searchName">
-    <ul>
-      <li v-for="name of filteredNames" :key="name.id">{{ name }}</li>
-    </ul>
-    
-    <hr>
-
-    <app-list></app-list>
+    <p>{{name}}</p>
   </div>
 
 </template>
 
 <script>
-import ListMixin from './listMixin'
 
 export default {
   data() {
     return {
-      title: 'Hello i am Vue.js'
+      name: ''
     }
-  },
-  mixins: [ListMixin]
+  }
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import List from './List.vue'
-
-Vue.component('app-list', List)
 
 new Vue({
   el: '#app',
