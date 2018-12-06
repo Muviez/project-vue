72b4fd7e41d39631113d0a3ee5a044522f7b3763
Andrey Goryushin <goryushin.andre@yandex.ru>
2018-12-06 13:49:08 +0300

    created personal control

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
   <div>
     <h2>From inputs</h2>
-    <input type="text" v-model.number="age">
+    
+    <app-onoff v-model="switched"></app-onoff>
 
-    <p>{{age}}</p>
+    <div>
+      <h3 v-if="switched">Command is enabled</h3>
+      <h3 v-else>Command is disbled</h3>
+    </div>
   </div>
 
 </template>
 
 <script>
+import Onoff from './OnOff.vue'
 
 export default {
   data() {
     return {
-      age: 20
+      switched: false
     }
   },
-  watch: {
-    age(value) {
-      console.log(value)
-      console.log(typeof value)
-    }
+  components: {
+    appOnoff: Onoff
   }
 }
 </script>
